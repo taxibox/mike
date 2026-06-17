f61a6a48b2b4a48aa19f29ba2bf54c076ca0113d
Erdinan <erdinanangkajaya@gmail.com>
2026-06-17 15:57:24 +1000

    fix login

--- frontend/src/lib/supabase.ts
+++ frontend/src/lib/supabase.ts
@@ -4,4 +4,8 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
 const supabaseAnonKey =
     process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY || "";
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+    auth: {
+        flowType: "pkce",
+    },
+});
